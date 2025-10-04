b48a0a430bbad161111dcc93a5ea212f1c815681
Ricardo Tanaka Terukina <112701774+rtanakatls@users.noreply.github.com>
2025-10-04 12:14:53 -0500

    Added auto targeting

--- Assets/Scripts/UnitShoot.cs
+++ Assets/Scripts/UnitShoot.cs
@@ -5,11 +5,11 @@ public class UnitShoot : MonoBehaviour
 {
     [SerializeField] private GameObject bulletPrefab;
     private float shootCooldown = 0.5f;
-    private UnitMovement unitMovement;
+    [SerializeField] private LayerMask targetLayerMask;
+    [SerializeField] private float radius;
 
     private void Awake()
     {
-        unitMovement = GetComponent<UnitMovement>();
         StartCoroutine(Shoot());
     }
 
@@ -17,11 +17,22 @@ public class UnitShoot : MonoBehaviour
     {
         while(true)
         {
-            GameObject obj = Instantiate(bulletPrefab);
-            obj.transform.position = transform.position;
-            obj.GetComponent<BulletMovement>().SetDirection(unitMovement.Direction);
+            Collider[] colliders = Physics.OverlapSphere(transform.position, radius, targetLayerMask);
+            if (colliders.Length > 0)
+            {
+                Vector3 direction = colliders[0].transform.position - transform.position;
+                GameObject obj = Instantiate(bulletPrefab);
+                obj.transform.position = transform.position;
+                obj.GetComponent<BulletMovement>().SetDirection(direction);
+            }
             yield return new WaitForSeconds(shootCooldown);
         }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, radius);
     }
 
 }
