150d6dd02ee2a78d9c2023367cb48bbaf059276e
Ricardo Tanaka Terukina <112701774+rtanakatls@users.noreply.github.com>
2025-11-08 13:50:04 -0500

    Added decal

--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -5,22 +5,28 @@ public class PlayerShoot : MonoBehaviour
 {
     [SerializeField] private LayerMask targetLayerMask;
     [SerializeField] private float distance;
+    [SerializeField] private GameObject decal;
+    private RaycastHit hit;
     private bool isLockOn;
     void Update()
     {
-
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.up, out hit, distance, targetLayerMask, QueryTriggerInteraction.UseGlobal))
+        isLockOn = Physics.Raycast(transform.position, transform.up, out hit, distance, targetLayerMask, QueryTriggerInteraction.UseGlobal);
+       
+        if (Input.GetMouseButtonDown(0))
         {
-            isLockOn = true;
-            if(Input.GetMouseButtonDown(0))
+            if (isLockOn)
             {
                 Destroy(hit.collider.gameObject);
             }
-        }
-        else
-        {
-            isLockOn = false;
+            else
+            {
+                RaycastHit missedHit;
+                if(Physics.Raycast(transform.position,transform.up,out missedHit, distance))
+                {
+                    GameObject obj = Instantiate(decal, missedHit.point + missedHit.normal * 0.1f,Quaternion.identity);
+                    obj.transform.forward = transform.up;
+                }
+            }
         }
     }
 
